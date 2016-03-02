f42ce046a7b04d2f4c83997fc7a1cab5bdd13274
oophub <tae.g.kim@colorado.edu>
2016-03-01 21:35:33 -0800

    20160301

---
+++ src/PacketAnalyzer/OConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketAnalyzer
+{
+    // Singleton class which contains configuration
+    class OConfiguration
+    {
+        // selected NIC device which will be used on packet capturing
+        private int _selectedNIC = -1;
+
+        private static OConfiguration _instance = null;
+        private OConfiguration() { }
+        public static OConfiguration instanceOf()
+        {
+            if (null == _instance)
+            {
+                _instance = new OConfiguration();
+            }
+            return _instance;
+        }
+
+        public int GetSelectedNIC()
+        {
+            return _selectedNIC;
+        }
+
+        public void SetSelectedNIC(int selected)
+        {
+            _selectedNIC = selected;
+        }
+
+    }
+}
